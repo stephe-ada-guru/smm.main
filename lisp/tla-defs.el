c9bea1b6673aa8c1e6506e96a002de3a52a8324f
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2007-12-06 10:05:19 +0000

    catch up to bzr

--- lisp/tla-defs.el
+++ lisp/tla-defs.el
@@ -1,6 +1,6 @@
 ;;; tla-defs.el --- UI Xtla's element definitions
 
-;; Copyright (C) 2003-2006 by all contributors
+;; Copyright (C) 2003-2007 by all contributors
 
 ;; Author: Stefan Reichoer, <stefan@xsteve.at>
 ;; Contributions from:
@@ -1856,10 +1856,6 @@ Hyperlink are sometimes long to set up with large ChangeLogs ..."
 ;; ----------------------------------------------------------------------------
 ;; Font lock keywords
 ;; ----------------------------------------------------------------------------
-
-(defvar dvc-log-edit-file-list-marker
-  "--This line, and those below, will be ignored--"
-  "A marker separating the actual log message from the list of files to commit.")
 
 ;;
 ;; Inventory file mode
