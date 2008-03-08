bfcf0d5b08d76cbbcba596a0ee58aac77c619f6a
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2008-03-08 16:54:28 +0000

    catch up to bzr.
    * texinfo/dvc-version.texinfo:
    * lisp/dvc-version.el:
    * dvc-load-install.el: should not be in cm

--- lisp/dvc-version.el
+++
@@ -1,4 +0,0 @@
-;; do not edit -- automatically generated file
-;; do not delete -- needed for bug reports
-(defconst dvc-version "stephen_leake@stephe-leake.org-20080308093748-ibvr4xd6kr1a5z00")
-(provide 'dvc-version)
