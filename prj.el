043ef4f693732fd7b92b6b68b2bf83d187d9bb53
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2015-08-08 12:54:14 +0000

    * prj.el: add missing paths

--- prj.el
+++ prj.el
@@ -2,7 +2,11 @@
 (jde-set-variables
  '(jde-global-classpath
    (list
-    "/Users/stephe/AppData/Local/Android/sdk/platforms/android-19/android.jar"))
+    "/Users/stephe/AppData/Local/Android/sdk/platforms/android-19/android.jar"
+    "/Projects/org.stephe_leake.music_player.java/app/build/intermediates/classes/release"
+;;    "/Projects/jdee/master/build/classes/" ;; jde tools (working copy)
+    "/Projects/jdee/emacs-master/build/classes/" ;; jde tools (working copy)
+    ))
  )
 
 (add-to-list 'jde-help-docsets '("Android" "http://developer.android.com/reference/" nil))
