13297ee1b316df69525a19eb1b4be4c367351a66
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2015-12-06 00:17:26 +0000

    * .mtn-ignore: add global db files
    * app/build.gradle (android): bump to 8
    
    * app/src/main/res/layout/main.xml: add vertical separators between notes buttons
    
    * app/src/main/res/layout/notification.xml: put tile on top
    
    * build/Makefile (VERSION): bump version
      (Local Variables): use random prj.el
    
    * prj.el: improve

--- prj.el
+++ prj.el
@@ -1,12 +1,49 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-global-classpath
-   (list
-    "/Users/stephe/AppData/Local/Android/sdk/platforms/android-19/android.jar"
-    "/Projects/org.stephe_leake.music_player.java/app/build/intermediates/classes/release"
-;;    "/Projects/jdee/master/build/classes/" ;; jde tools (working copy)
-    "/Projects/jdee/emacs-master/build/classes/" ;; jde tools (working copy)
-    ))
- )
+;; random elisp project file
 
-(add-to-list 'jde-help-docsets '("Android" "http://developer.android.com/reference/" nil))
+(require 'cedet-global)
+(require 'ede-files-patches) ;; find-file-project ede, ede-find-file
+(require 'ede/generic) ;; generic-autoloader
+(require 'project-patches)   ;; project-find-file
+(require 'project-menu)
+;; (require 'project-multi-lang)
+;; (require 'project-ml-global)
+
+(add-to-list 'ede-locate-setup-options 'ede-locate-global)
+
+(sal-check-ede "Generic Monotone")
+
+(unless (project-try-ede ".")
+  (error "EDE not enabled properly"))
+
+;; Set the EDE locator object directly (not via
+;; ede-enable-locate-on-project), to select ede-locate-global-path and
+;; set the path.
+
+(let ((root (file-name-directory (or #$ default-directory)))
+      (prj (ede-toplevel)))
+
+  (oset prj locate-obj
+	(ede-locate-global-path
+	 :root (expand-file-name ".")
+	 :path-iter
+	 (make-path-iterator
+	 :user-path-non-recursive
+	 (list
+	  root ;; prj.el, design.text, build.gradle
+	  (concat root "build") ;; main Makefile
+	  (concat root "app/src/main") ;; AndroidManifest
+	  )
+	  ;; "app/src/main/java" accessed via Gnu global
+	 :user-path-recursive
+	 (list (concat root "app/src/main/res"))
+	 )))
+
+  (add-to-list 'project-find-functions 'project-menu-prj)
+  (add-to-list 'project-find-functions 'project-try-ede)
+
+  (add-to-list 'project-menu-list (cons "Stephe's Music main" prj))
+
+  (project-menu-select "Stephe's Music main")
+)
+
+;; end of file
