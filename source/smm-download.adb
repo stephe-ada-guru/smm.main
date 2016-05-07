8d5c153a72850228f95236e6431228fa160ad1f1
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2016-05-07 16:23:13 +0000

    * source/smm-download.adb (Download_Album_Aux): fix typo

--- source/smm-download.adb
+++ source/smm-download.adb
@@ -62,7 +62,7 @@ is
    begin
       Search
         (Directory => Source_Dir,
-         Pattern   => "AlbumAux*.jpg",
+         Pattern   => "AlbumArt*.jpg",
          Filter    => (Ordinary_File => True, others => False),
          Process   => Copy_Aux'Access);
 
