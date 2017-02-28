8d5f4ffa8582466ad6057d44277b19d5669d86c2
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2017-02-28 18:02:25 +0000

    * source/smm-history.adb (): split histogram into vocal, instrumental. add more bins. set x labels properly.
    * build/Makefile (music): new

--- source/smm-history.adb
+++ source/smm-history.adb
@@ -1,7 +1,12 @@
 --  Abstract :
 --
 --  Report average, standard deviation of download period (last to
---  previous). Also output histogram plot in gnuplot files.
+--  previous) for 'vocal' and 'instrumental' categories. Also output
+--  histogram plot in gnuplot files.
+--
+--  References:
+--
+--  gnu plot manual:    /usr/share/doc/gnuplot/manual/gnuplot.pdf
 --
 --  Copyright (C) 2016 - 2017 Stephen Leake.  All Rights Reserved.
 --
@@ -35,82 +40,98 @@ is
 
    Stats : Float_Stats.Stats_Type;
 
+   Years_Per_Bin : constant Float := 0.5;
+
    function To_Bin (Item : in Float) return Integer
    is begin
-      return Integer (Item);
+      return Integer (Item / Years_Per_Bin);
    end To_Bin;
 
-   Bin_Max : constant := 10; -- years
+   Bin_Max : constant := 10;
 
    package Histograms is new SAL.Gen_Histogram
-     (Bin_Min => -1,
+     (Bin_Min => 0,
       Bin_Max => Bin_Max,
       Value   => Float,
       To_Bin  => To_Bin);
 
-   Histogram        : Histograms.Object;
-   Dont_Play        : Integer := 0;
-   Never_Downloaded : Integer := 0;
-   Downloaded_Once  : Integer := 0;
+   package Histogram_Plots is new Histograms.Gen_Gnuplot (Years_Per_Bin);
 
-   package Histogram_Plots is new Histograms.Gen_Gnuplot;
-
-   procedure Accumulate (I : in Iterator_Type)
+   procedure Do_Category (Category : in String)
    is
-      use Ada.Float_Text_IO;
+      Histogram        : Histograms.Object;
+      Dont_Play        : Integer := 0;
+      Never_Downloaded : Integer := 0;
+      Downloaded_Once  : Integer := 0;
 
-      Last   : constant Time_Type := Read_Last_Downloaded (Db, I);
-      Prev   : constant Time_Type := Read_Prev_Downloaded (Db, I);
-      Period : constant Float     := Float ((Last - Prev) / Seconds_Per_Year);
-   begin
-      if Read (Db, I, Category_Key, Missing_Key => Ignore) = "dont_play" then
-         Dont_Play := Dont_Play + 1;
-         return;
-      end if;
-
-      if Last = 0.0 then
-         Never_Downloaded := Never_Downloaded + 1;
-
-      elsif Prev = 0.0 then
-         Downloaded_Once := Downloaded_Once + 1;
-
-      else
-         Stats.Accumulate (Period);
-         Histogram.Accumulate (Period);
-
-         if To_Bin (Period) > Bin_Max then
-            Put ("song:" & Current (I) & " period:");
-            Put (Period, Aft => 2, Exp => 0);
-            New_Line;
+      procedure Accumulate (I : in Iterator_Type)
+      is
+         Item_Category : constant String := Read (Db, I, Category_Key, Missing_Key => Ignore);
+      begin
+         if Category /= Item_Category then
+            return;
+         elsif Category = "dont_play" then
+            Dont_Play := Dont_Play + 1;
+            return;
          end if;
-      end if;
-   end Accumulate;
 
-   I : Iterator_Type := First (Db, Songs_Key);
-begin
+         declare
+            use Ada.Float_Text_IO;
 
-   loop
-      exit when Is_Null (I);
-      Accumulate (I);
-      Next (I);
-   end loop;
+            Last   : constant Time_Type := Read_Last_Downloaded (Db, I);
+            Prev   : constant Time_Type := Read_Prev_Downloaded (Db, I);
+            Period : constant Float     := Float ((Last - Prev) / Seconds_Per_Year);
+         begin
+            if Last = 0.0 then
+               Never_Downloaded := Never_Downloaded + 1;
 
-   declare
-      use Ada.Float_Text_IO;
-      Display : constant Float_Stats.Display_Type := Stats.Display;
+            elsif Prev = 0.0 then
+               Downloaded_Once := Downloaded_Once + 1;
+
+            else
+               Stats.Accumulate (Period);
+               Histogram.Accumulate (Period);
+
+               if To_Bin (Period) > Bin_Max then
+                  Put ("song:" & Current (I) & " period:");
+                  Put (Period, Aft => 2, Exp => 0);
+                  New_Line;
+               end if;
+            end if;
+         end;
+      end Accumulate;
+
+      I : Iterator_Type := First (Db, Songs_Key);
    begin
-      Put_Line ("last/prev downloaded stats (years):");
-      Put_Line ("songs            :" & Integer'Image (Display.Count));
-      Put_Line ("dont_play        :" & Integer'Image (Dont_Play));
-      Put_Line ("never downloaded :" & Integer'Image (Never_Downloaded));
-      Put_Line ("downloaded once  :" & Integer'Image (Downloaded_Once));
-      Put ("mean    :"); Put (Display.Mean, Fore => 3, Aft => 2, Exp => 0); New_Line;
-      Put ("std dev :"); Put (Display.Standard_Deviation, Fore => 3, Aft => 2, Exp => 0); New_Line;
-      Put ("min     :"); Put (Display.Min, Fore => 3, Aft => 2, Exp => 0); New_Line;
-      Put ("max     :"); Put (Display.Max, Fore => 3, Aft => 2, Exp => 0); New_Line;
-   end;
+      New_Line;
+      Put_Line (Category & " :");
+      loop
+         exit when Is_Null (I);
+         Accumulate (I);
+         Next (I);
+      end loop;
 
-   Histogram_Plots.Put_Plot (Histogram, "histogram.dat", "histogram.gnuplot", "years");
+      declare
+         use Ada.Float_Text_IO;
+         Display : constant Float_Stats.Display_Type := Stats.Display;
+      begin
+         Put_Line ("songs            :" & Integer'Image (Display.Count));
+         Put_Line ("dont_play        :" & Integer'Image (Dont_Play));
+         Put_Line ("never downloaded :" & Integer'Image (Never_Downloaded));
+         Put_Line ("downloaded once  :" & Integer'Image (Downloaded_Once));
+         Put_Line ("last/prev downloaded stats (years):");
+         Put ("mean    :"); Put (Display.Mean, Fore => 3, Aft => 2, Exp => 0); New_Line;
+         Put ("std dev :"); Put (Display.Standard_Deviation, Fore => 3, Aft => 2, Exp => 0); New_Line;
+         Put ("min     :"); Put (Display.Min, Fore => 3, Aft => 2, Exp => 0); New_Line;
+         Put ("max     :"); Put (Display.Max, Fore => 3, Aft => 2, Exp => 0); New_Line;
+      end;
 
-   Put_Line ("do: 'gnuplot histogram.gnuplot > histogram.png'");
+      Histogram_Plots.Put_Plot
+        (Histogram, Category, "years");
+
+   end Do_Category;
+
+begin
+   Do_Category ("vocal");
+   Do_Category ("instrumental");
 end SMM.History;
