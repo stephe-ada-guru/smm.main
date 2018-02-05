7d63c11c51f79bb6ce857ed8cb953e726bbb26e1
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2018-02-05 17:37:13 +0000

    * source/smm-server.adb (Handle_Download, Handle_Meta, Handle_File)   (Handle_Put_Notes): new, factored out of Handle_Request

--- source/smm-server.adb
+++ source/smm-server.adb
@@ -45,7 +45,218 @@ package body SMM.Server is
    Source_Root : Ada.Strings.Unbounded.Unbounded_String; -- does not end in /
    Db_Filename : Ada.Strings.Unbounded.Unbounded_String;
 
-   function Handle_Request (Request : AWS.Status.Data) return AWS.Response.Data
+   function Handle_Download (URI : in AWS.URL.Object) return AWS.Response.Data
+   is
+      use Ada.Containers;
+      use Ada.Exceptions;
+      use Ada.Strings.Unbounded;
+      use AWS.URL;
+      use SAL.Config_Files;
+      use Song_Lists;
+
+      Category   : constant String     := Parameter (URI, "category");
+      Count      : constant Count_Type := Count_Type'Value (Parameter (URI, "count"));
+      New_Count  : constant Count_Type := Count_Type'Value (Parameter (URI, "new_count"));
+
+      Seed_Param : constant String     := Parameter (URI, "seed"); -- only used in unit tests
+      Seed       : constant Integer    :=
+        (if Seed_Param'Length > 0 then Integer'Value (Seed_Param) else 0);
+
+      Over_Select_Ratio_Param : constant String := Parameter (URI, "over_select_ratio");
+      Over_Select_Ratio       : constant Float  :=
+        (if Over_Select_Ratio_Param'Length > 0 then Float'Value (Over_Select_Ratio_Param) else 2.0);
+
+      Db       : SAL.Config_Files.Configuration_Type;
+      Songs    : List;
+      Response : Unbounded_String;
+   begin
+      Open
+        (Db,
+         -Db_Filename,
+         Missing_File          => Raise_Exception,
+         Duplicate_Key         => Raise_Exception,
+         Read_Only             => False,
+         Case_Insensitive_Keys => True);
+
+      Least_Recent_Songs
+        (Db, Category, Songs,
+         Song_Count        => Count,
+         New_Song_Count    => New_Count,
+         Over_Select_Ratio => Over_Select_Ratio,
+         Seed              => Seed);
+
+      for I of Songs loop
+         Response :=  Response & Normalize (Read (Db, I, File_Key)) & ASCII.CR & ASCII.LF;
+      end loop;
+
+      Close (Db);
+
+      return AWS.Response.Build ("text/plain", Response);
+   exception
+   when E : others =>
+      if Is_Open (Db) then
+         Close (Db);
+      end if;
+      return AWS.Response.Acknowledge
+        (Status_Code  => AWS.Messages.S500,
+         Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
+   end Handle_Download;
+
+   function Handle_Meta (URI : in AWS.URL.Object) return AWS.Response.Data
+   is
+      use Ada.Directories;
+      use Ada.Strings.Unbounded;
+      use AWS.URL;
+
+      Source_Dir : constant String := -Source_Root & Path (URI);
+      Response   : Unbounded_String;
+
+      procedure Copy_Aux (Dir_Ent : in Directory_Entry_Type)
+      is begin
+         Response := Response &
+           Relative_Name (-Source_Root, Normalize (Full_Name (Dir_Ent))) & ASCII.CR & ASCII.LF;
+      end Copy_Aux;
+   begin
+      Search
+        (Directory => Source_Dir,
+         Pattern   => "AlbumArt*.jpg",
+         Filter    => (Ordinary_File => True, others => False),
+         Process   => Copy_Aux'Access);
+
+      Search
+        (Directory => Source_Dir,
+         Pattern   => "liner_notes.pdf",
+         Filter    => (Ordinary_File => True, others => False),
+         Process   => Copy_Aux'Access);
+
+      return AWS.Response.Build ("text/plain", Response);
+   exception
+   when Ada.IO_Exceptions.Name_Error =>
+      --  GNAT runtime sets message to "(unknown directory "")"; no file name!
+      raise Ada.IO_Exceptions.Name_Error with "unknown directory '" & Path (URI) & "'";
+
+   end Handle_Meta;
+
+   function Handle_File (URI : in AWS.URL.Object) return AWS.Response.Data
+   is
+      use Ada.Directories;
+      use Ada.Exceptions;
+      use AWS.URL;
+      use SAL.Config_Files;
+
+      Filename  : constant String := -Source_Root & Path (URI) & File (URI);
+      Ext       : constant String := Extension (Filename);
+      Mime_Type : constant String :=
+        --  MIME types from https://www.iana.org/assignments/media-types/media-types.xhtml
+        --  also GNAT/share/examples/aws/web_elements/mime.types
+        (if    Ext = "jpg" then "image/jpeg"
+         elsif Ext = "mp3" then "audio/mpeg"
+         elsif Ext = "pdf" then "application/pdf"
+         else "");
+
+      Db : Configuration_Type;
+      Result : AWS.Response.Data;
+      I : SAL.Config_Files.Iterator_Type;
+      Prev_Downloaded : SAL.Time_Conversions.Extended_ASIST_Time_String_Type;
+
+   begin
+      if Mime_Type'Length = 0 then
+         return AWS.Response.Acknowledge
+           (Status_Code  => AWS.Messages.S500,
+            Message_Body => "<p>file extension '" & Ext & "' not supported.");
+      end if;
+
+      if Exists (Filename) then
+         if Ext = "mp3" then
+            Open
+              (Db,
+               -Db_Filename,
+               Missing_File          => Raise_Exception,
+               Duplicate_Key         => Raise_Exception,
+               Read_Only             => False,
+               Case_Insensitive_Keys => True);
+
+            --  Find does not want leading / on filename
+            I := Find (Db, Filename (Length (Source_Root) + 2 .. Filename'Last));
+
+            if I = Null_Iterator then
+               return AWS.Response.Acknowledge
+                 (Status_Code  => AWS.Messages.S500,
+                  Message_Body => "file not found");
+            end if;
+
+            Write_Last_Downloaded (Db, I, SAL.Time_Conversions.To_TAI_Time (Ada.Calendar.Clock));
+
+            Prev_Downloaded := SAL.Time_Conversions.To_Extended_ASIST_String (Read_Prev_Downloaded (Db, I));
+
+            Close (Db);
+         end if;
+
+         Result := AWS.Response.File (Mime_Type, Filename);
+         AWS.Response.Set.Add_Header (Result, "X-prev_downloaded", Prev_Downloaded);
+         return Result;
+
+      else
+         return AWS.Response.Acknowledge
+           (Status_Code  => AWS.Messages.S404,
+            Message_Body => "<p>file '" & Filename & "' not found.");
+      end if;
+   exception
+   when Name_Error =>
+      --  Exists raises Name_Error if the directory does not exist
+      return AWS.Response.Acknowledge
+        (Status_Code  => AWS.Messages.S404,
+         Message_Body => "<p>file '" & Filename & "' not found.");
+   when E : others =>
+      if Is_Open (Db) then
+         Close (Db);
+      end if;
+      return AWS.Response.Acknowledge
+        (Status_Code  => AWS.Messages.S500,
+         Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
+   end Handle_File;
+
+   function Handle_Put_Notes (Request : in AWS.Status.Data; URI : in AWS.URL.Object) return AWS.Response.Data
+   is
+      use Ada.Directories;
+      use Ada.Exceptions;
+      use Ada.Strings.Fixed;
+      use Ada.Text_IO;
+      use AWS.URL;
+
+      Pathname : constant String := -Source_Root & Path (URI);
+      Filename : constant String := Pathname & File (URI);
+      Data     : constant String := -AWS.Status.Binary_Data (Request);
+      First    : Integer         := Data'First;
+      Last     : Integer;
+      File     : File_Type;
+   begin
+      if not Exists (Pathname) then
+         Create_Directory (Pathname);
+      end if;
+
+      if Exists (Filename) then
+         Open (File, Append_File, Filename);
+      else
+         Create (File, Out_File, Filename);
+      end if;
+      loop
+         Last := Index (Source => Data (First .. Data'Last), Pattern => ASCII.CR & ASCII.LF);
+         exit when Last < Data'First;
+         Put_Line (File, Data (First .. Last - 1));
+         First := Last + 2;
+      end loop;
+      Close (File);
+
+      return AWS.Response.Acknowledge (Status_Code  => AWS.Messages.S200);
+   exception
+   when E : others =>
+      return AWS.Response.Acknowledge
+        (Status_Code  => AWS.Messages.S500,
+         Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
+   end Handle_Put_Notes;
+
+   function Handle_Request (Request : in AWS.Status.Data) return AWS.Response.Data
    is
       use Ada.Exceptions;
       use AWS.Response;
@@ -56,208 +267,18 @@ package body SMM.Server is
       case Method (Request) is
       when GET =>
          if File (URI) = "download" then
-            declare
-               use Ada.Containers;
-               use Ada.Strings.Unbounded;
-               use SAL.Config_Files;
-               use Song_Lists;
-
-               Category   : constant String     := Parameter (URI, "category");
-               Count      : constant Count_Type := Count_Type'Value (Parameter (URI, "count"));
-               New_Count  : constant Count_Type := Count_Type'Value (Parameter (URI, "new_count"));
-
-               Seed_Param : constant String     := Parameter (URI, "seed"); -- only used in unit tests
-               Seed       : constant Integer    :=
-                 (if Seed_Param'Length > 0 then Integer'Value (Seed_Param) else 0);
-
-               Over_Select_Ratio_Param : constant String := Parameter (URI, "over_select_ration");
-               Over_Select_Ratio       : constant Float  :=
-                 (if Over_Select_Ratio_Param'Length > 0 then Float'Value (Over_Select_Ratio_Param) else 2.0);
-
-               Db       : SAL.Config_Files.Configuration_Type;
-               Songs    : List;
-               Response : Unbounded_String;
-            begin
-               Open
-                 (Db,
-                  -Db_Filename,
-                  Missing_File          => Raise_Exception,
-                  Duplicate_Key         => Raise_Exception,
-                  Read_Only             => False,
-                  Case_Insensitive_Keys => True);
-
-               Least_Recent_Songs
-                 (Db, Category, Songs,
-                  Song_Count        => Count,
-                  New_Song_Count    => New_Count,
-                  Over_Select_Ratio => Over_Select_Ratio,
-                  Seed              => Seed);
-
-               for I of Songs loop
-                  Response :=  Response & Normalize (Read (Db, I, File_Key)) & ASCII.CR & ASCII.LF;
-               end loop;
-
-               Close (Db);
-
-               return Build ("text/plain", Response);
-            exception
-            when E : others =>
-               if Is_Open (Db) then
-                  Close (Db);
-               end if;
-               return Acknowledge
-                 (Status_Code  => AWS.Messages.S500,
-                  Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
-            end;
+            return Handle_Download (URI);
 
          elsif File (URI) = "meta" then
-            declare
-               use Ada.Directories;
-               use Ada.Strings.Unbounded;
-               Source_Dir : constant String := -Source_Root & Path (URI);
-               Response   : Unbounded_String;
-
-               procedure Copy_Aux (Dir_Ent : in Directory_Entry_Type)
-               is begin
-                  Response := Response &
-                    Relative_Name (-Source_Root, Normalize (Full_Name (Dir_Ent))) & ASCII.CR & ASCII.LF;
-               end Copy_Aux;
-            begin
-               Search
-                 (Directory => Source_Dir,
-                  Pattern   => "AlbumArt*.jpg",
-                  Filter    => (Ordinary_File => True, others => False),
-                  Process   => Copy_Aux'Access);
-
-               Search
-                 (Directory => Source_Dir,
-                  Pattern   => "liner_notes.pdf",
-                  Filter    => (Ordinary_File => True, others => False),
-                  Process   => Copy_Aux'Access);
-
-               return Build ("text/plain", Response);
-            exception
-            when Ada.IO_Exceptions.Name_Error =>
-               --  GNAT runtime sets message to "(unknown directory "")"; no file name!
-               raise Ada.IO_Exceptions.Name_Error with "unknown directory '" & Path (URI) & "'";
-            end;
+            return Handle_Meta (URI);
 
          else
             --  It's a file request.
-            declare
-               use Ada.Directories;
-               use SAL.Config_Files;
-
-               Filename  : constant String := -Source_Root & Path (URI) & File (URI);
-               Ext       : constant String := Extension (Filename);
-               Mime_Type : constant String :=
-                 --  MIME types from https://www.iana.org/assignments/media-types/media-types.xhtml
-                 --  also GNAT/share/examples/aws/web_elements/mime.types
-                 (if    Ext = "jpg" then "image/jpeg"
-                  elsif Ext = "mp3" then "audio/mpeg"
-                  elsif Ext = "pdf" then "application/pdf"
-                  else "");
-
-               Db : Configuration_Type;
-               Result : AWS.Response.Data;
-               I : SAL.Config_Files.Iterator_Type;
-               Prev_Downloaded : SAL.Time_Conversions.Extended_ASIST_Time_String_Type;
-
-            begin
-               if Mime_Type'Length = 0 then
-                  return Acknowledge
-                    (Status_Code  => AWS.Messages.S500,
-                     Message_Body => "<p>file extension '" & Ext & "' not supported.");
-               end if;
-
-               if Exists (Filename) then
-                  if Ext = "mp3" then
-                     Open
-                       (Db,
-                        -Db_Filename,
-                        Missing_File          => Raise_Exception,
-                        Duplicate_Key         => Raise_Exception,
-                        Read_Only             => False,
-                        Case_Insensitive_Keys => True);
-
-                     --  Find does not want leading / on filename
-                     I := Find (Db, Filename (Length (Source_Root) + 2 .. Filename'Last));
-
-                     if I = Null_Iterator then
-                        return Acknowledge
-                          (Status_Code  => AWS.Messages.S500,
-                           Message_Body => "file not found");
-                     end if;
-
-                     Write_Last_Downloaded (Db, I, SAL.Time_Conversions.To_TAI_Time (Ada.Calendar.Clock));
-
-                     Prev_Downloaded := SAL.Time_Conversions.To_Extended_ASIST_String (Read_Prev_Downloaded (Db, I));
-
-                     Close (Db);
-                  end if;
-
-                  Result := File (Mime_Type, Filename);
-                  AWS.Response.Set.Add_Header (Result, "X-prev_downloaded", Prev_Downloaded);
-                  return Result;
-
-               else
-                  return Acknowledge
-                    (Status_Code  => AWS.Messages.S404,
-                     Message_Body => "<p>file '" & Filename & "' not found.");
-               end if;
-            exception
-            when Name_Error =>
-               --  Exists raises Name_Error if the directory does not exist
-               return Acknowledge
-                 (Status_Code  => AWS.Messages.S404,
-                  Message_Body => "<p>file '" & Filename & "' not found.");
-            when E : others =>
-               if Is_Open (Db) then
-                  Close (Db);
-               end if;
-               return Acknowledge
-                 (Status_Code  => AWS.Messages.S500,
-                  Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
-            end;
+            return Handle_File (URI);
          end if;
 
       when PUT =>
-         --  Notes
-         declare
-            use Ada.Directories;
-            use Ada.Strings.Fixed;
-            use Ada.Text_IO;
-            Pathname : constant String := -Source_Root & Path (URI);
-            Filename : constant String := Pathname & File (URI);
-            Data     : constant String := -Binary_Data (Request);
-            First    : Integer         := Data'First;
-            Last     : Integer;
-            File     : File_Type;
-         begin
-            if not Exists (Pathname) then
-               Create_Directory (Pathname);
-            end if;
-
-            if Exists (Filename) then
-               Open (File, Append_File, Filename);
-            else
-               Create (File, Out_File, Filename);
-            end if;
-            loop
-               Last := Index (Source => Data (First .. Data'Last), Pattern => ASCII.CR & ASCII.LF);
-               exit when Last < Data'First;
-               Put_Line (File, Data (First .. Last - 1));
-               First := Last + 2;
-            end loop;
-            Close (File);
-
-            return Acknowledge (Status_Code  => AWS.Messages.S200);
-         exception
-         when E : others =>
-            return Acknowledge
-              (Status_Code  => AWS.Messages.S500,
-               Message_Body => "exception " & Exception_Name (E) & ": " & Exception_Message (E));
-         end;
+         return Handle_Put_Notes (Request, URI);
 
       when others =>
          return Acknowledge
